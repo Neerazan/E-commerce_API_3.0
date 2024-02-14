d1eff7f0f09a1071926de1698f173df65774bd32
Neerajan <nirajandhakal634@gmail.com>
2024-02-14 16:12:48 +0545

    authenticating the user in pytest

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -16,3 +16,17 @@ class TestCreateCollection:
 
         #Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+    def test_if_user_is_admin_returns_403(self):
+        #Arrange -> Empty in this case
+
+        #Act
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post('/store/collections/', {
+            'title': 'a'
+        })
+
+        #Assert
+        assert response.status_code == status.HTTP_403_FORBIDDEN
