b115c60e8f99acf94a00a366b21a71a0ea4d7891
Neerajan <nirajandhakal634@gmail.com>
2024-02-14 09:26:20 +0545

    creating and executing tasks using celery

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.core.mail import EmailMessage, send_mail, mail_admins, BadHeaderError
-from templated_mail.mail import BaseEmailMessage
+from .tasks import notify_customers
+# from django.core.mail import EmailMessage, send_mail, mail_admins, BadHeaderError
+# from templated_mail.mail import BaseEmailMessage
 
 
 def say_hello(request):
-    try:
+    # try:
         # send_mail('This is Subject', 'This is Message', 'from@nirajan.com', ['bob@nirajan.com'])
 
         #Send mail to admin
@@ -13,14 +14,17 @@ def say_hello(request):
         # 
         
         #Templated Mail
-        message = BaseEmailMessage(
-            template_name='emails/hello.html',
-            context={
-                'name': 'Nirajan'
-            }
-        )
-        message.send(['nirajandhakal@gmail.com'])
-    except:
-        pass
+    #     message = BaseEmailMessage(
+    #         template_name='emails/hello.html',
+    #         context={
+    #             'name': 'Nirajan'
+    #         }
+    #     )
+    #     message.send(['nirajandhakal@gmail.com'])
+    # except:
+    #     pass
+
+    # Code for celery
+    notify_customers.delay('This is test message..')
 
     return render(request, 'hello.html', {'name': 'Mosh'})
