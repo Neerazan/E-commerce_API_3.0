9900807d033cb46f6cf5cba58706ab3998005e88
Neerajan <nirajandhakal634@gmail.com>
2024-02-14 14:33:07 +0545

    small change

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,14 +1,16 @@
 from rest_framework.test import APIClient
 from rest_framework import status
+import pytest
 
 
+@pytest.mark.django_db
 class TestCreateCollection:
-    def test_if_user_is_anonymous_returns_401():
+    def test_if_user_is_anonymous_returns_401(self):
         #Arrange -> Empty in this case
 
         #Act
         client = APIClient()
-        response = client.post('store/collections', {
+        response = client.post('/store/collections/', {
             'title': 'a'
         })
 
