4c6233f21579f5418d706fa0c853670dc929506b
Neerajan <nirajandhakal634@gmail.com>
2024-02-15 22:48:11 +0545

    apply chache in class base view

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,15 @@
 from django.core.cache import cache
 from django.views.decorators.cache import cache_page
+from rest_framework.views import APIView
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 import requests
 # from .tasks import notify_customers
 # from django.core.mail import EmailMessage, send_mail, mail_admins, BadHeaderError
 # from templated_mail.mail import BaseEmailMessage
 
-@cache_page(5*60)
-def say_hello(request):
+# @cache_page(5*60)
+# def say_hello(request):
     # try:
         # send_mail('This is Subject', 'This is Message', 'from@nirajan.com', ['bob@nirajan.com'])
 
@@ -32,7 +34,17 @@ def say_hello(request):
 
 
     #Code for caching
-    response = requests.get('https://httpbin.org/delay/3')
-    data = response.json()
+    # response = requests.get('https://httpbin.org/delay/3')
+    # data = response.json()
 
-    return render(request, 'hello.html', {'name': data})
+    # return render(request, 'hello.html', {'name': data})
+
+
+
+#Cache in Class Base view
+class HelloView(APIView):
+    @method_decorator(cache_page(5*60))
+    def get(self, request):
+        response = requests.get('https://httpbin.org/delay/3')
+        data = response.json()
+        return render(request, 'hello.html', {'name': data})
